7cc7798c8c39da85500cce479d94d1d22fb677be
Tom Corley <tomcorleycarpentry@gmail.com>
2013-11-14 21:56:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carpentry::Application.config.secret_key_base = 'a5df6d0f28da22e30e3a36e0f9d5a6ebaf05767014727eddac17beb5eb0b1177f0ef96c5cc5f02c857f07f2484058adaca418c300a5ed4df2cc22b9792f73582'
